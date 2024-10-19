23a16e7e2bb694a5286607345662db07f413e73b
unknown <stifshin@willamette.edu>
2024-10-19 00:21:03 -0700

    component folder added

---
+++ components/NavBar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+const Navbar = () =>{
+    return (
+        <View>
+
+            <Text>
+                ok im goated
+            </Text>
+        </View>
+
+
+
+    );
+
+
+};
+
+const styles = StyleSheet.create({
+
+
+
+});
+
+
+export default Navbar;
