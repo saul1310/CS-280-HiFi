49b31b4ba38313992e3b0a5845377df97ebac4b9
unknown <stifshin@willamette.edu>
2024-10-23 23:27:40 -0700

    image placement on home screen, colorselection started

--- Pages/Community.tsx
+++ Pages/Community.tsx
@@ -1,15 +1,34 @@
 // Pages/Community.tsx
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import Navbar from '../components/NavBar';
 
-const Community = () => {
+const Community = ({ navigation }) => {
   return (
-    <View>
-       <Navbar navigation={navigation} /> 
-      <Text>Welcome to the Community page!</Text>
+    <View style={styles.container}>
+      <Navbar navigation={navigation} />
+      <Text style={styles.text}>Welcome to the Community page!</Text>
+      <View style={styles.redBox}></View> {/* Added red box */}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center', // Center content vertically
+    alignItems: 'center', // Center content horizontally
+    backgroundColor: '#F8F4E8',
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  redBox: {
+    width: 150, // Box width
+    height: 150, // Box height
+    backgroundColor: '#0d2410', // Red background
+  },
+});
 
 export default Community;
