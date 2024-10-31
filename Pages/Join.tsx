09d93daa68f3a3583fdfa3eee1c9dbaf8ef7b035
unknown <stifshin@willamette.edu>
2024-10-30 22:20:33 -0700

    added clans

--- Pages/Join.tsx
+++ Pages/Join.tsx
@@ -14,9 +14,78 @@ const Join = ({ navigation }) => {
 
             <View style={styles.GroupContainer}>
                 <View style={styles.Group}>
+                
                    
-                    <Text>Group Details Here</Text>
+                    <Text style = {styles.Clan_info}>Cycle Fiends </Text>
+                    <View style = {styles.Join_button}>
+                        <Text style = {styles.Join_text}> Join </Text>
+                        </View>
+                    <Text style = {styles.Clan_altinfo}> 2/12</Text>
                 </View>
+                <View style={styles.Group}>
+                
+                   
+                <Text style = {styles.Clan_info}>Pedal Pushers </Text>
+                <View style = {styles.Join_button}>
+                    <Text style = {styles.Join_text}> Join </Text>
+                    </View>
+                <Text style = {styles.Clan_altinfo}> 8/12</Text>
+            </View>
+            <View style={styles.Group}>
+                
+                   
+                <Text style = {styles.Clan_info}>Cycle Tribe</Text>
+                <View style = {styles.Join_button}>
+                    <Text style = {styles.Join_text}> Join </Text>
+                    </View>
+                <Text style = {styles.Clan_altinfo}> 6/12</Text>
+            </View>
+            <View style={styles.Group}>
+                
+                   
+                <Text style = {styles.Clan_info}>Trail Blazers </Text>
+                <View style = {styles.Join_button}>
+                    <Text style = {styles.Join_text}> Join </Text>
+                    </View>
+                <Text style = {styles.Clan_altinfo}> 12/12</Text>
+            </View>
+            <View style={styles.Group}>
+                
+                   
+                <Text style = {styles.Clan_info}>The Spin Squad </Text>
+                <View style = {styles.Join_button}>
+                    <Text style = {styles.Join_text}> Join </Text>
+                    </View>
+                <Text style = {styles.Clan_altinfo}> 7/12</Text>
+            </View>
+
+            <View style={styles.Group}>
+                
+                   
+                <Text style = {styles.Clan_info}>Road Warriors</Text>
+                <View style = {styles.Join_button}>
+                    <Text style = {styles.Join_text}> Join </Text>
+                    </View>
+                <Text style = {styles.Clan_altinfo}> 5/12</Text>
+            </View>
+            <View style={styles.Group}>
+                
+                   
+                <Text style = {styles.Clan_info}>Chain Gang </Text>
+                <View style = {styles.Join_button}>
+                    <Text style = {styles.Join_text}> Join </Text>
+                    </View>
+                <Text style = {styles.Clan_altinfo}> 6/12</Text>
+            </View>
+            <View style={styles.Group}>
+                
+                   
+                <Text style = {styles.Clan_info}>Cycle Fiends </Text>
+                <View style = {styles.Join_button}>
+                    <Text style = {styles.Join_text}> Join </Text>
+                    </View>
+                <Text style = {styles.Clan_altinfo}> 2/12</Text>
+            </View>
             </View>
         </View>
     );
@@ -57,10 +126,12 @@ const styles = StyleSheet.create({
 
     Group: {
         width: '100%',         
-        height: 60,       
+        height: 60,      
+        justifyContent: 'space-between',
         backgroundColor: '#01b1fe',
+        flexDirection: 'row',
         borderRadius: 10,    
-        marginVertical: 10, 
+        marginVertical: 2, 
         padding: 10,        
         shadowColor: '#000',  
         shadowOffset: { width: 0, height: 2 }, 
@@ -68,6 +139,35 @@ const styles = StyleSheet.create({
         shadowRadius: 4,    
     
     },
+    Clan_info : {
+        color: '#000',
+        alignSelf : 'flex-start',
+
+    },
+    Clan_altinfo : {
+        color: '#000',
+        alignSelf : 'flex-end',
+
+
+
+    },
+
+    Join_button : {
+        height :35,
+        width :80,
+        borderRadius :25,
+        backgroundColor : '#f5bf14',
+        shadowColor: '#000',  
+        shadowOffset: { width: 0, height: 2 }, 
+        shadowOpacity: 0.3,   
+        shadowRadius: 4,    
+
+
+    },
+    Join_text : {
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 });
 
 export default Join;
